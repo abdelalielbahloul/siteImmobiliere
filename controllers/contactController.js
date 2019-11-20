18cd639e4aac6067247e6c93e5f49501b0a97ee3
Abdelali <bahloul97abdel@gmail.com>
2019-11-20 17:36:25 +0100

    add return to home route in master page and return an array of contacts to  test design

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -2,12 +2,25 @@ const mongoose = require('mongoose');
 const edge = require('edge.js');
 
 exports.index = (req, res) => {
+    const [contacts] = [];
     res.status(200).send(edge.render('contact', { 
-        contacts: {
-            fullName: 'EL BAHLOUL Abdelali',
-            email: 'elbahloulabdelali@gmail.com',
-            message: 'Voici un message que j\'ai vous a laisser sur la boite mail'
-        }
+        contacts: [
+            {
+                fullName: 'EL BAHLOUL Abdelali1',
+                email: 'elbahloulabdelali@gmail.com',
+                message: 'Voici un message que j\'ai vous a laisser sur la boite mail'
+            },
+            {
+                fullName: 'EL BAHLOUL Abdelali2',
+                email: 'elbahloulabdelali@gmail.com',
+                message: 'Voici un message que j\'ai vous a laisser sur la boite mail'
+            },
+            {
+                fullName: 'EL BAHLOUL Abdelali3',
+                email: 'elbahloulabdelali@gmail.com',
+                message: 'Voici un message que j\'ai vous a laisser sur la boite mail'
+            }
+        ]
     }));
 }
 
